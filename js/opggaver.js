2bce25d0b4657fe17283db24f9aea257bdb26ad3
Stfa75 <u2535501@rts-365.dk>
2026-02-03 08:47:33 +0100

    Opgave 1

---
+++ js/opggaver.js
@@ -0,0 +1,11 @@
+let person = {
+firstname: "Steffen",
+lastname: "Fabricius",
+age: "50",
+hometown: "Frederiksberg", 
+}
+
+console.log(person.firstname)
+console.log(person.age)
+console.log(person["hometown"])
+console.log(person["firstname"])
