4b2d8bfbbde4c83c0f9b3f54fbdb398e270aa862
Stfa75 <u2535501@rts-365.dk>
2026-02-03 09:18:12 +0100

    Opgave 2

--- js/opggaver.js
+++ js/opggaver.js
@@ -1,11 +1,31 @@
 let person = {
-firstname: "Steffen",
-lastname: "Fabricius",
-age: "50",
-hometown: "Frederiksberg", 
+    firstname: "Steffen",
+    lastname: "Fabricius",
+    age: "50",
+    hometown: "Frederiksberg",
 }
 
 console.log(person.firstname)
 console.log(person.age)
 console.log(person["hometown"])
 console.log(person["firstname"])
+
+/*******Opgave 2*****/
+
+let mig = {
+    firstname: "Steffen",
+    lastname: "Fabricius",
+    age: "50",
+    hometown: "Frederiksberg",
+    married: false,
+    hobbies: ["webdesign", "computerspil", "historie"],
+    pet: {
+        petname: "Bobby",
+        Type: "snail"
+    }
+}
+console.log(mig.hobbies);
+
+mig.hobbies.forEach(function (hobby) {
+    console.log(hobby)
+})
