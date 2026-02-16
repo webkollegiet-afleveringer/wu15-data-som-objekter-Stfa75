bdbf9b838e490d5d4be1cb0fcdcc38a7813d8302
Stfa75 <u2535501@rts-365.dk>
2026-02-16 10:56:04 +0100

    opgave version 3

--- js/index.js
+++ js/index.js
@@ -21,6 +21,8 @@ let heroIcon = document.createElement("img");
 heroIcon.setAttribute("src", hero.icon);
 let heroButton = document.createElement("button")
 heroButton.textContent = "Eksplore"
+heroButton.style.fontSize = "40px"
+heroButton.classList.add("knaptext")
 heroButton.append(heroIcon)
 heroSection.append(heroDiv, hsection);
 heroDiv.append(heroImg);
@@ -51,50 +53,79 @@ services.forEach(service => {
 /*********Facilities sektion********/
 
 let facilityHeadline = document.createElement("h1");
-    facilityHeadline.textContent = `${facilities.headline}`;
+facilityHeadline.textContent = `${facilities.headline}`;
 let flexDiv = document.createElement("div");
-    flexDiv.classList.add("flexwrapper");
-    facilitiesSection.append(facilityHeadline, flexDiv);
+flexDiv.classList.add("flexwrapper");
+facilitiesSection.append(facilityHeadline, flexDiv);
 
-    facilities.options.forEach(facility => {
+facilities.options.forEach(facility => {
 
-let faDiv = document.createElement("div")
+    let faDiv = document.createElement("div")
     faDiv.classList.add("faoptions")
-let facImg = document.createElement("img");
+    let facImg = document.createElement("img");
     facImg.setAttribute("src", facility.icon);
-let facHeadline = document.createElement("h2");
+    let facHeadline = document.createElement("h2");
     facHeadline.textContent = `${facility.headline}`;
-let facText = document.createElement("p");
+    let facText = document.createElement("p");
     facText.textContent = `${facility.text}`;
-    faDiv.append(facImg,facHeadline,facText);
-    flexDiv.append(faDiv); 
+    faDiv.append(facImg, facHeadline, facText);
+    flexDiv.append(faDiv);
 })
 /******Sites sektionen*****/
 
-let sitesDiv = document.createElement("div");
-
+let community = document.createElement("div")
+community.classList.add("join")
 let siteHeadline = document.createElement("h2");
 siteHeadline.textContent = `${sites.headline}`;
 let siteText = document.createElement("p");
-siteHeadline.textContent = `${sites.text}`;
+siteText.textContent = `${sites.text}`;
 let siteIcon = document.createElement("img");
 siteIcon.setAttribute("src", sites.btnicon);
+// sitesSection.append(siteHeadline, siteText, siteIcon)
 
-sites.places.forEach(container => {
+const joinString =/*html*/
+    `
+    <section class="join">
+        <h2>${sites.headline}</h2>
+        <p>${sites.text}</p>
+        <button class="join-button">
+            Join Now
+            <img src="${sites.btnicon}" alt="Arrow icon"></button>
+    </section>
+`
+
+sitesSection.insertAdjacentHTML("afterbegin", joinString)
+
+// sites.places.forEach(container => {
+
+//     let destination = document.createElement("div")
+//     destination.classList.add("places")
+//     let siteImg = document.createElement("img");
+//     siteImg.setAttribute("src", container.img);
+//     /*husk at det altid er parameteret (når tingene skal lokaliseres)
+//      du skal referere til når foreach udfyldes*/
+//     let cityName = document.createElement("h3");
+//     cityName.textContent = `${container.name}`;
+//     let city = document.createElement("p");
+//     city.textContent = `${container.city}`;
+//     // destination.append(siteImg, cityName, city)
+//     // community.append(destination)
+
+const placeString = /*html*/
+    `
+    <section class= "place">
+   ${sites.places.map(place => `
+        <div class="place-card">
+            <img src="${place.img}" alt="${place.name}">
+            <h3>${place.name}</h3>
+            <h4>${place.city}</h4>
+        </div>
+    `).join('')}
+</section>
+`
+sitesSection.insertAdjacentHTML("afterbegin", placeString);
 
 
-    let siteImg = document.createElement("img");
-    siteImg.setAttribute("src", container.img);
-    /*husk at det altid er parameteret (når tingene skal lokaliseres)
-     du skal referere til når foreach udfyldes*/
-    let cityName = document.createElement("h3");
-    cityName.textContent = `${container.name}`;
-    let city = document.createElement("p");
-    city.textContent = `${container.city}`;
-    sitesSection.append(sitesDiv, siteHeadline, siteText, siteIcon, cityName, city)
-    sitesDiv.append(siteImg);
-
-})
 /***********Advantage Sektionen******/
 
 let advantageDiv = document.createElement("div");
@@ -104,9 +135,9 @@ advantages.forEach(container2 => {
     let advIcon = document.createElement("img")
     advIcon.setAttribute("src", container2.icon);
     let advantageHeadline = document.createElement("h2");
-    advantageHeadline.textContent = `${container2.headline}`;
+    advantageHeadline.textContent = `${container2.headline} `;
     let advText = document.createElement("p");
-    advText.textContent = `${container2.text}`;
+    advText.textContent = `${container2.text} `;
     advantage.append(advantageDiv, advantageHeadline, advText)
     advantageDiv.append(advIcon);
 })
@@ -116,25 +147,25 @@ advantages.forEach(container2 => {
 let footerDiv = document.createElement("div");
 
 let footerHeadlineOne = document.createElement("h3");
-footerHeadlineOne.textContent = `${footer.headline}`;
+footerHeadlineOne.textContent = `${footer.headline} `;
 let footerText0 = document.createElement("h1");
-footerText0.textContent = `${footer.text0}`;
+footerText0.textContent = `${footer.text0} `;
 footerSection.append(footerDiv, footerHeadlineOne, footerText0)
 
 footer.info.forEach(container3 => {
 
     let footerHeadline2 = document.createElement("h3");
-    footerHeadline2.textContent = `${container3.headline1}`;
+    footerHeadline2.textContent = `${container3.headline1} `;
     let firstText = document.createElement("p");
-    firstText.textContent = `${container3.text}`;
+    firstText.textContent = `${container3.text} `;
     let secondText = document.createElement("p");
-    secondText.textContent = `${container3.text2}`;
+    secondText.textContent = `${container3.text2} `;
     let thirdText = document.createElement("p");
-    thirdText.textContent = `${container3.text3}`;
+    thirdText.textContent = `${container3.text3} `;
     let textFour = document.createElement("p");
-    textFour.textContent = `${container3.text4}`;
+    textFour.textContent = `${container3.text4} `;
     let textFive = document.createElement("p");
-    textFive.textContent = `${container3.text5}`;
+    textFive.textContent = `${container3.text5} `;
     footerSection.append(footerDiv, footerHeadline2, firstText, secondText, thirdText, textFour, textFive)
 
     let ulOne = document.createElement("ul");
